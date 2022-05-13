6b2c22a65320af138f4556bfb7c10c3c06b891d9
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-05-13 10:17:25 +0700

    membuat reusable modalcard components & menambahkan element modal pada file html

---
+++ src/components/UI/ModalCard.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { createPortal } from "react-dom";
+
+const ModalBackground = (props) => {
+  return (
+    <div className="bg-gradient-to-b from-slate-800 to-transparent fixed top-0 bottom-0 left-0 right-0"></div>
+  );
+};
+
+const ModalContent = (props) => {
+  const classes = `${props.className} fixed top-5 left-1/2 -translate-1/2 bg-slate-200 rounded-lg shadow-lg z-30`;
+
+  return <div className={classes}>{props.children}</div>;
+};
+
+const modal = document.getElementById("modal");
+
+const ModalCard = (props) => {
+  return (
+    <React.Fragment>
+      {(createPortal(<ModalContent className={props.className} />), modal)}
+      {createPortal(<ModalBackground />, modal)}
+    </React.Fragment>
+  );
+};
+
+export default ModalCard;
