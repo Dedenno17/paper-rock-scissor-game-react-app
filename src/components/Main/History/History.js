67ff2639e54587688673438df37ede9141dc7d27
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-05-16 12:32:34 +0700

    memperbaiki navbar style & table history

--- src/components/Main/History/History.js
+++ src/components/Main/History/History.js
@@ -4,18 +4,22 @@ const History = (props) => {
   return (
     <div className="w-full bg-slate-200 py-3 px-4">
       <table className="m-auto w-full text-sm">
-        <tr>
-          <th className="bg-primaryPurple text-slate-200 border-slate-700 border-[1px] p-1">
-            Your Chioce
-          </th>
-          <th className="bg-primaryPurple text-slate-200 border-slate-700 border-[1px] p-1">
-            Computer's Choice
-          </th>
-          <th className="bg-primaryPurple text-slate-200 border-slate-700 border-[1px] p-1">
-            Win
-          </th>
-        </tr>
-        <HistoryRow />
+        <thead>
+          <tr>
+            <th className="bg-primaryPurple text-slate-200 border-slate-700 border-[1px] p-1">
+              Your Chioce
+            </th>
+            <th className="bg-primaryPurple text-slate-200 border-slate-700 border-[1px] p-1">
+              Computer's Choice
+            </th>
+            <th className="bg-primaryPurple text-slate-200 border-slate-700 border-[1px] p-1">
+              Win
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <HistoryRow />
+        </tbody>
       </table>
     </div>
   );
